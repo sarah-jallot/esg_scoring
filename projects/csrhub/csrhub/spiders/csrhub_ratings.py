9e7d451ec1d5fedd9f2edafd7d55b60e868b5836
Sarah Jallot <sarah.jallot@hec.edu>
2021-04-23 00:38:53 +0200

    CsrHub

--- projects/csrhub/csrhub/spiders/csrhub_ratings.py
+++ projects/csrhub/csrhub/spiders/csrhub_ratings.py
@@ -5,7 +5,8 @@ import scrapy
 class CsrhubRatingsSpider(scrapy.Spider):
     name = 'csrhub_ratings'
     allowed_domains = ['www.csrhub.com/CSR_and_sustainability_information']
-    start_urls = ['http://www.csrhub.com/CSR_and_sustainability_information/']
+    start_urls = ['https://www.csrhub.com/CSR_and_sustainability_information/Orange-SA']
 
     def parse(self, response):
-        pass
+        company_section = response.xpath('//div[@class="company-section_top-rank_num"]/span/span[@class="value"]/text()')
+        company_section.xpath("./")
